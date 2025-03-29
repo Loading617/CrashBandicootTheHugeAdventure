9ed9194e31e7ba41b4a27ddb3c4427a5b993a541
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-03-29 14:56:20 -0400

    Delete texture.cpp

--- texture.cpp
+++
@@ -1,9 +0,0 @@
-SDL_Texture* loadTexture(const char* filename) {
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP(filename));
-    return texture;
-}
-
-void renderGame(SDL_Texture* texture) {
-    SDL_RenderCopy(renderer, texture, NULL, NULL); // Draw texture to screen
-    SDL_RenderPresent(renderer);
-}
